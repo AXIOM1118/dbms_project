5a8ad59a9130b69403be161140ed6e5fee77f443
AXIOM1118 <axiomtaye11@gmail.com>
2026-04-13 00:25:34 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="axiom1811",
+        password="",
         database="log_integrity_db"
     )
 
